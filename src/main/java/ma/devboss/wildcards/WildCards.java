6ead5b7ffcbd56f71957a557a99f0f26224ff75b
devbosslocal <ysaber201@outlook.com>
2023-12-04 01:30:58 +0100

    WildCards Definitions

--- src/main/java/ma/devboss/wildcards/WildCards.java
+++ src/main/java/ma/devboss/wildcards/WildCards.java
@@ -1,11 +1,20 @@
 package ma.devboss.wildcards;
 
-import ma.devboss.impirative_vs_declarative.Main;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WildCards {
+    // <?> any object
+
+
+    // <? extends SomeClass> /*  Upper bound */ ===> SomeClass object itself or any class extends or implement SomeClass but not the ParentClasses
+    // ********* [From SomeClass To the last Child]
+
+
+    // <? super Some Class>  /*  Lower bound */ ===> SomeClass object itself or the classes that its implement but not the subclasses that implement SomeClass
+    // ********* [From SomeClass To the First Parent]
+
+
+
     public static void main(String[] args) {
         DataProvider pd = new PeopleDataProvider();
         List<Person> list = (List<Person>) pd.provide();
@@ -19,4 +28,6 @@ public class WildCards {
                 .toList()
                 .forEach(System.out::print);
     }
+
+
 }
