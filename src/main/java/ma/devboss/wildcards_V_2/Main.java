2050c30f554e3b20ae53359f009ac3ee65c26544
devbosslocal <ysaber201@outlook.com>
2023-12-06 00:53:39 +0100

    nothing 2

--- src/main/java/ma/devboss/wildcards_V_2/Main.java
+++ src/main/java/ma/devboss/wildcards_V_2/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // start
         DataProvider dataProvider = new PersonDataProvider();
         List<Person> people  = (List<Person>) dataProvider.provide();
         people.forEach(System.out::print);
