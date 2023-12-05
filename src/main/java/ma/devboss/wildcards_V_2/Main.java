f1abfe606c8e4f98473904e3a0621c4f3553ccd9
local <ysaber201@hotmail.com>
2023-12-06 00:33:38 +0100

    buisiness

--- src/main/java/ma/devboss/wildcards_V_2/Main.java
+++ src/main/java/ma/devboss/wildcards_V_2/Main.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello java fonctional");
         DataProvider dataProvider = new PersonDataProvider();
         List<Person> people  = (List<Person>) dataProvider.provide();
         people.forEach(System.out::print);
