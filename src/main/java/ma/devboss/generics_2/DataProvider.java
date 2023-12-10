19e284fc37993eeb50d4ecfb2ffda8d11f436bfd
devbosslocal <ysaber201@outlook.com>
2023-12-10 23:36:16 +0100

    updating generics_2 package

--- src/main/java/ma/devboss/generics_2/DataProvider.java
+++ src/main/java/ma/devboss/generics_2/DataProvider.java
@@ -2,7 +2,7 @@ package ma.devboss.generics_2;
 
 import java.util.List;
 
-public abstract class DataProvider <T>{
+public abstract class DataProvider<T>{
     public DataProvider() {}
     public abstract List<T> provide();
 }
--- src/main/java/ma/devboss/generics_2/Main.java
+++ src/main/java/ma/devboss/generics_2/Main.java
@@ -6,9 +6,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         showData(new PersonDataProvider());
-
-
-
     }
     public static void showData(DataProvider<Person> data){
         List<Person> people  = data.provide();
