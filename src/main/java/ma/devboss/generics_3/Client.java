bb1162dda7e9d1c6fbd35dd66a475fc843172705
devbosslocal <ysaber201@outlook.com>
2023-12-11 02:49:22 +0100

    Updating  generics_3 1.2

--- src/main/java/ma/devboss/generics_3/Client.java
+++ src/main/java/ma/devboss/generics_3/Client.java
@@ -2,17 +2,14 @@ package ma.devboss.generics_3;
 
 public class Client {
     public static void main(String[] args) {
-        DataProvider<Product> listOfProducts = new ProductDataProvider();
-        listOfProducts
-                .provideData()
-                .forEach(p->System.out.println(p.getName()));
-
         getProductInfo(new ProductDataProvider());
-
     }
+
+
+
     public static void getProductInfo(DataProvider<Product> dataProvider){
-        System.out.println("#################");
+        System.out.println("######### About All Products ########");
         dataProvider.provideData()
-                .forEach(p->System.out.println(p.getName()));
+                .forEach(System.out::println);
     }
 }
