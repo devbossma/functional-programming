c2496539142d8707fc7894b618c98c07527d63f3
devbosslocal <ysaber201@outlook.com>
2023-12-12 01:17:21 +0100

    The initial Set-Up of the component package

---
+++ src/main/java/ma/devboss/componentArchetecture/product/internal/entity/Product.java
@@ -0,0 +1,26 @@
+package ma.devboss.componentArchetecture.product.internal.entity;
+
+import ma.devboss.generics_3.Entity;
+
+public class Product implements Entity {
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
