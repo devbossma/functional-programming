653dd7c224ae276afe4687fe7d3ec1f7446f7974
devbosslocal <ysaber201@outlook.com>
2023-12-17 00:45:29 +0100

    java stream implimentation

--- src/main/java/ma/devboss/functionalInterfaces/streams/_Stream.java
+++ src/main/java/ma/devboss/functionalInterfaces/streams/_Stream.java
@@ -1,6 +1,7 @@
 package ma.devboss.functionalInterfaces.streams;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,7 +19,7 @@ public class _Stream {
                 new Product("Dell Inspiration 15 puce", "Computers", 6500.97, true),
                 new Product("Dell Inspiration 13 puce", "Computers", 6500.97, false),
                 new Product("SONY 360", "Camera", 4800.97, true),
-                new Product("GoPro", "Camera", 6700.97, true),
+                new Product("GoPro", "Camera", 600.97, true),
                 new Product("Canon", "Camera", 9700.97, false)
                 );
         // Filter takes a predicate
@@ -45,6 +46,13 @@ public class _Stream {
         List<Product> products1 = sorted
                 .toList();
         System.out.println(products1);
+
+        System.out.println("------------------------------");
+
+        productList.stream()
+                .takeWhile(product -> product.getPrice() >= 2_800.97)
+                .forEach(System.out::println);
+
 
 
 
