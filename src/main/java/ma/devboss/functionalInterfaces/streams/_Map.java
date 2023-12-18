2faa6e4e3c16d5677a5436f4029a7c84de18bbcc
devbosslocal <ysaber201@outlook.com>
2023-12-18 23:04:14 +0100

    java stream implimentation 2.1

--- src/main/java/ma/devboss/functionalInterfaces/streams/_Map.java
+++ src/main/java/ma/devboss/functionalInterfaces/streams/_Map.java
@@ -7,8 +7,22 @@ import java.util.stream.Collectors;
 public class _Map {
     public static void main(String[] args) {
         List<Product> productList = ProductDataProvider.productList;
-        Set<String> categories = productList.stream().map(Product::getCategory).collect(Collectors.toSet());
-        System.out.println(categories);
+//        System.out.println(productList);
 
+        Set<String> categories = productList.stream()
+                .map(Product::getCategory)
+                .collect(Collectors.toSet());
+//      System.out.println(categories);
+
+
+
+        List<Product> promotions = productList.stream()
+                .filter(Product::getInStock)
+                .filter(product -> product.getPrice() > 6_000d)
+                .map(product -> new Product(product.getName(),
+                        product.getCategory(), product.getPrice() - 100d,
+                        product.getInStock()))
+                .toList();
+//        System.out.println(promotions);
     }
 }
