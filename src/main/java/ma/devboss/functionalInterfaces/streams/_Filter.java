0589043d0ca947789516ebbd0a01cdc2822401fc
devbosslocal <ysaber201@outlook.com>
2023-12-18 01:17:32 +0100

    java stream implimentation 1.5

--- src/main/java/ma/devboss/functionalInterfaces/streams/_Filter.java
+++ src/main/java/ma/devboss/functionalInterfaces/streams/_Filter.java
@@ -1,11 +1,16 @@
 package ma.devboss.functionalInterfaces.streams;
 
-import java.util.List;
+
+import java.util.function.Predicate;
 
 public class _Filter {
     public static void main(String[] args) {
+        /* filtering based on a specific condition or a Predicate */
+        Predicate<Product> inStock = Product::getInStock;
+        // with stream methode we go up to the abstraction mode
         ProductDataProvider.productList.stream()
-                .filter(Product::getInStock)
+                .filter(inStock)
+                .filter(p->p.getPrice() > 5000) // we can apply more filters on other filter as well
                 .forEach(System.out::println);
 
     }
